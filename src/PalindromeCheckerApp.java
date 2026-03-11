5be5d5a5f7c03543fe4817863825317175fe2e31
Chaitanya303212 <chaitanyapotnuru006@gmail.com>
2026-03-11 19:10:25 +0530

    UC12: Strategy pattern for palindrome algorithms

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,25 +1,61 @@
-import java.util.Scanner;
+import java.util.*;
 
-// Palindrome service class
-class PalindromeChecker {
+// Strategy Interface
+interface PalindromeStrategy {
+    boolean checkPalindrome(String text);
+}
 
-    // Method to check palindrome
+// Stack Strategy
+class StackStrategy implements PalindromeStrategy {
+
     public boolean checkPalindrome(String text) {
 
-        int left = 0;
-        int right = text.length() - 1;
+        Stack<Character> stack = new Stack<>();
 
-        while (left < right) {
+        for (char c : text.toCharArray()) {
+            stack.push(c);
+        }
 
-            if (text.charAt(left) != text.charAt(right)) {
+        for (char c : text.toCharArray()) {
+            if (c != stack.pop())
                 return false;
-            }
-
-            left++;
-            right--;
         }
 
         return true;
+    }
+}
+
+// Deque Strategy
+class DequeStrategy implements PalindromeStrategy {
+
+    public boolean checkPalindrome(String text) {
+
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char c : text.toCharArray()) {
+            deque.addLast(c);
+        }
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast())
+                return false;
+        }
+
+        return true;
+    }
+}
+
+// Context class
+class PalindromeService {
+
+    private PalindromeStrategy strategy;
+
+    public PalindromeService(PalindromeStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public boolean execute(String text) {
+        return strategy.checkPalindrome(text);
     }
 }
 
@@ -29,17 +65,27 @@ public class PalindromeCheckerApp {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter a string: ");
+        System.out.println("Enter text:");
         String text = sc.nextLine();
 
-        // Create object of service class
-        PalindromeChecker checker = new PalindromeChecker();
+        System.out.println("Choose Algorithm:");
+        System.out.println("1. Stack Strategy");
+        System.out.println("2. Deque Strategy");
 
-        boolean result = checker.checkPalindrome(text);
+        int choice = sc.nextInt();
 
-        if (result)
+        PalindromeStrategy strategy;
+
+        if (choice == 1)
+            strategy = new StackStrategy();
+        else
+            strategy = new DequeStrategy();
+
+        PalindromeService service = new PalindromeService(strategy);
+
+        if (service.execute(text))
             System.out.println("Palindrome");
         else
-            System.out.println("Not a Palindrome");
+            System.out.println("Not Palindrome");
     }
 }
